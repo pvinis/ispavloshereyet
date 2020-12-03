e602f52e6a592dfd3e939a36da9c3682c6a0771a
Pavlos Vinieratos <pvinis@gmail.com>
2020-12-03 00:04:33 +0000

    getting there

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import {DateTime} from "luxon"
+import humanizeDuration from "humanize-duration"
+import {useState, useEffect } from "react"
+import {useSpring, animated} from 'react-spring'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const target = DateTime.local(2020, 12, 23, 12, 32).setZone('Europe/Amsterdam', {keepLocalTime: true})
+
+
+export const App=()=> {
+	const [ticker, updateTicker] = useState(0)
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			updateTicker(t => t+1)
+		}, 1000)
+		return () => clearInterval(intervalId)
+	}, [])
+
+	const [targetHasPast, setTargetHasPast] = useState(target <= DateTime.local())
+	const [diff, setDiff] = useState(humanizeDuration(target.diffNow().as("milliseconds"), {largest: 2, maxDecimalPoints: 0}))
+
+	const updateStuff = () => {
+		setTargetHasPast(target <= DateTime.local())
+		setDiff(humanizeDuration(target.diffNow().as("milliseconds"), {largest: 2, maxDecimalPoints: 0, units: ["d", "h", "m", "s"]}))
+	}
+
+	useEffect(() => updateStuff(), [ticker])
+
+	const color = targetHasPast ? "#77dd77" : "#ff6961"
+
+	const props = useSpring({fontSize: ticker % 2 === 0 ? 60 : 50 })
+
+	return (
+		<div style={{position: "absolute", top: 0, bottom: 0, left: 0, right: 0, backgroundColor: color, display: 'flex', alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+			{targetHasPast ? (
+					<p style={{fontSize: 80, fontFamily: 'Iosevka Web', fontWeight: 900}}>YES!!</p>
+			) : (
+				<>
+					<p style={{fontSize: 60, fontFamily: 'Iosevka Web', fontWeight: 500, transform: "rotate(8deg)"}}>Not yet,</p>
+					<p style={{fontSize: 40, fontFamily: 'Iosevka Web'}}>
+						<span>but only </span>
+						<animated.span style={{fontWeight: 400, fontSize: props.fontSize}}>{diff}</animated.span>
+						<span> left</span>
+					</p>
+				</>
+			)}
+		</div>
+	)
 }
-
-export default App;
