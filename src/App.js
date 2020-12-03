ca1849f1a5b4889f44effdc8a0895fc194f0f023
Pavlos Vinieratos <pvinis@gmail.com>
2020-12-03 00:44:53 +0000

    ugh bad code :p

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ const target = DateTime.local(2020, 12, 23, 12, 32).setZone('Europe/Amsterdam', 
 
 export const App=()=> {
 	const [ticker, updateTicker] = useState(0)
+	const [slowerTicker, updateSlowerTicker] = useState(0)
 	useEffect(() => {
 		const intervalId = setInterval(() => {
 			updateTicker(t => t+1)
+			if (ticker % 2 === 0) {
+				updateSlowerTicker(t => t+1)
+			}
 		}, 1000)
 		return () => clearInterval(intervalId)
-	}, [])
+	}, [ticker])
 
 	const [targetHasPast, setTargetHasPast] = useState(target <= DateTime.local())
 	const [diff, setDiff] = useState(humanizeDuration(target.diffNow().as("milliseconds"), {largest: 2, maxDecimalPoints: 0}))
@@ -28,18 +32,28 @@ export const App=()=> {
 
 	const color = targetHasPast ? "#77dd77" : "#ff6961"
 
-	const props = useSpring({fontSize: ticker % 2 === 0 ? 60 : 50 })
+	const {x} = useSpring({x: ticker % 2 === 0 ? 5 : 0.5, config: {duration: 1000}})
+	const {slowX} = useSpring({slowX: slowerTicker % 2 === 0 ? 1 : 0, config: {duration: 400}})
 
 	return (
 		<div style={{position: "absolute", top: 0, bottom: 0, left: 0, right: 0, backgroundColor: color, display: 'flex', alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
 			{targetHasPast ? (
-					<p style={{fontSize: 80, fontFamily: 'Iosevka Web', fontWeight: 900}}>YES!!</p>
+					<animated.p style={{fontSize: 80, fontFamily: 'Iosevka Web', fontWeight: 900,
+						transform: x.interpolate(x => `scale(${x})`),
+					}}>YES!!</animated.p>
 			) : (
 				<>
 					<p style={{fontSize: 60, fontFamily: 'Iosevka Web', fontWeight: 500, transform: "rotate(8deg)"}}>Not yet,</p>
 					<p style={{fontSize: 40, fontFamily: 'Iosevka Web'}}>
 						<span>but only </span>
-						<animated.span style={{fontWeight: 400, fontSize: props.fontSize}}>{diff}</animated.span>
+						<animated.span style={{
+							fontWeight: 400,
+							fontSize: slowX
+								.interpolate({
+									range:  [ 0, 0.25, 0.50, 0.75,  1],
+									output: [50,   52,   50,   52, 50],
+        						})
+						}}>{diff}</animated.span>
 						<span> left</span>
 					</p>
 				</>
