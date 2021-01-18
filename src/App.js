80ca1a08432bfaf6acc440c2428a77d5d50109f6
Pavlos Vinieratos <pvinis@gmail.com>
2021-01-18 14:35:12 +0200

    .

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export const App=()=> {
 	}, [])
 
 
-	const color = "magenta"
+	const color = "black"
 ///// add my face as favicon and app manifest thing
 
 	const fontSizes= isPhone ? {
@@ -31,7 +31,7 @@ small: 26,
 
 	return (
 		<div style={{position: "absolute", top: 0, bottom: 0, left: 0, right: 0, backgroundColor: color, display: 'flex', alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-			<p style={{fontFamily: "Iosevka Web"}}>💝 Not right now, but soon again 💝</p>
+			<p style={{fontFamily: "Iosevka Web"}}>😶</p>
 		</div>
 	)
 }
