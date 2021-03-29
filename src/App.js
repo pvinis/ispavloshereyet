8a182e1148e4c1ba787c8037d7554a8d609f9d84
Pavlos Vinieratos <pvinis@gmail.com>
2021-03-29 01:18:44 +0100

    k

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ small: 26,
 
 	return (
 		<div style={{position: "absolute", top: 0, bottom: 0, left: 0, right: 0, backgroundColor: color, display: 'flex', alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-					<p style={{fontFamily: "Iosevka Web" , color: "black"}}>If your day is not a good day, remember I'm there if you need me or just want to see a nice face.</p>
+					<p style={{fontFamily: "Iosevka Web" , color: "white"}}>If your day is not a good day, remember I'm there if you need me or just want to see a nice face.</p>
 		</div>
 	)
 }
