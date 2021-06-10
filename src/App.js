7dea09a5f7dfd246c64822696e35ffee7d9a6f81
Pavlos Vinieratos <pvinis@gmail.com>
2021-06-10 20:33:46 +0300

    gg

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export const App=()=> {
 	}, [])
 
 
-	const color = "darkblue"
+	const color = "hotpink"
 ///// add my face as favicon and app manifest thing
 
 	const fontSizes= isPhone ? {
@@ -31,7 +31,7 @@ small: 26,
 
 	return (
 		<div style={{position: "absolute", top: 0, bottom: 0, left: 0, right: 0, backgroundColor: color, display: 'flex', alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-					<p style={{fontFamily: "Iosevka Web" , color: "white"}}>If your day is not a good day, remember I'm there if you need me or just want to see a nice face.</p>
+					<p style={{fontFamily: "Iosevka Web" , color: "white"}}>FUCK YEA HE IS!! (on Monday 😁)</p>
 		</div>
 	)
 }
