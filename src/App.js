5a167f6372c1498892497c44a5054691b6688681
Pavlos Vinieratos <pvinis@gmail.com>
2021-08-08 12:11:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export const App=()=> {
 	}, [])
 
 
-	const color = "hotpink"
+	const color = "yellow"
 ///// add my face as favicon and app manifest thing
 
 	const fontSizes= isPhone ? {
@@ -31,7 +31,7 @@ small: 26,
 
 	return (
 		<div style={{position: "absolute", top: 0, bottom: 0, left: 0, right: 0, backgroundColor: color, display: 'flex', alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
-					<p style={{fontFamily: "Iosevka Web" , color: "white"}}>FUCK YEA HE IS!! (on Monday 😁)</p>
+					<p style={{fontFamily: "Iosevka Web" , color: "darkblue"}}>only a text away</p>
 		</div>
 	)
 }
