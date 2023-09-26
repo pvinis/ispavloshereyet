427d3556bcaa04404ebed2c545c107509c4d7a4b
Pavlos Vinieratos <pvinis@gmail.com>
2023-09-26 18:10:04 +0100

    donno

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { App } from "./App"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-expect-error
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
