22e5e6eeb15146653c74421d4708d5c9515c7ede
Pavlos Vinieratos <pvinis@gmail.com>
2023-09-26 17:53:08 +0100

    iosevka

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import locationJson from "./location.json"
 
 export function App() {
 	return (
-		<div className="absolute w-full h-full bg-slate-600 flex items-center justify-center flex-col">
+		<div className="absolute w-full h-full bg-slate-600 flex items-center justify-center flex-col font-mono">
 			<ol className="text-gray-400">
 				{locationJson["version-1"].locations.map((location, index) => (
 					<li key={index} className="first:text-white">
