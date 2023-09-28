9b244c846b7a5aac5cfa5c270d3f9a20fa941c50
Pavlos Vinieratos <pvinis@gmail.com>
2023-09-28 15:59:15 +0100

    location init

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react"
+import * as TaskManager from "expo-task-manager"
 import { Octokit } from "@octokit/rest"
 import { Button, Text, TextInput, View } from "react-native"
+import {
+	Accuracy,
+	LocationObject,
+	getCurrentPositionAsync,
+	reverseGeocodeAsync,
+	startLocationUpdatesAsync,
+	useBackgroundPermissions,
+	useForegroundPermissions,
+} from "expo-location"
 
 const octokit = new Octokit({ auth: process.env.EXPO_PUBLIC_GH_TOKEN })
+
+const LOCATION_TASK_NAME = "background-location-task"
 
 export default function Page() {
 	const [text, setText] = useState("")
@@ -17,7 +29,22 @@ export default function Page() {
 		}
 		doIt()
 	})
-	///// check location change
+
+	const [fgStatus, fgRequestPermission] = useForegroundPermissions()
+	const [bgStatus, bgRequestPermission] = useBackgroundPermissions()
+
+	useEffect(() => {
+		const doIt = async () => {
+			if (!fgStatus?.granted) await fgRequestPermission()
+			if (!bgStatus?.granted) await bgRequestPermission()
+
+			// const loc = await getCurrentPositionAsync()
+			// console.log("WOWOW", loc.coords)
+			await startLocationUpdatesAsync(LOCATION_TASK_NAME, { accuracy: Accuracy.Lowest })
+			///when do we stopupdates?
+		}
+		doIt()
+	}, [fgStatus?.granted, bgStatus?.granted])
 
 	return (
 		<View>
@@ -48,3 +75,27 @@ export default function Page() {
 		</View>
 	)
 }
+
+TaskManager.defineTask<{ locations: LocationObject[] }>(
+	LOCATION_TASK_NAME,
+	({ data: { locations }, error }) => {
+		if (error) {
+			console.log("error", error.message)
+			return
+		}
+
+		if (locations) {
+			console.log("locations", locations)
+			locations.forEach(async (loc, index) => {
+				const rev = await reverseGeocodeAsync(loc.coords)
+				console.log("loc", index, loc, rev[0].city, rev[0].country)
+				if (rev.length > 1) {
+					console.log("rev has more than one", rev)
+				}
+			})
+		}
+	}
+)
+
+/// sentry/segment?
+/// make release testflight
